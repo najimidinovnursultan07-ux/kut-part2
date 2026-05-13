21010f277dc6f6222878aec56b17b3d4afad9146
Nursultan <najimidinovnursultan07@gmail.com>
2026-05-13 11:03:16 +0600

    озгоруу

---
+++ java.js
@@ -0,0 +1,13 @@
+const kubik = document.querySelector(".box")
+const btn = document.querySelector(".btn")
+kubik.style.background = "white"
+
+
+function getcolor() {
+kubik.style.backgroundColor = "red"
+
+}
+
+btn.onclick = getcolor
+
+
